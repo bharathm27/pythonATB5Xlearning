45fefc8984953d754bb3c80c1ded6531bd6a091f
bharathm <bharathk745@gmail.com>
2024-11-08 20:28:52 +0530

    7 NOV

---
+++ NOVEMBER/05112024PYBASICS/LAB001_HELLOWORLD.py
@@ -0,0 +1,8 @@
+print("hello world")
+print("THIS IS BHARATH",999,sep='***',end='\n')
+print("Happy to be the testing academy part",sep='***')
+print(5 + 5, 9 * 6, " " " ", 8 / 3)
+print("Hi", "bye", "sir")
+print("Testing", 555 ,"Academy by pro mode",123,"pramod ji",sep='*/@',end='*****')
+print(111,222,333)
+print("END")
---
+++ NOVEMBER/05112024PYBASICS/LAB002.py
@@ -0,0 +1,2 @@
+print("I LOVE MY INDIA")
+print("123 cricket")
